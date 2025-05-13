4cc0a27acd854a598fa66157c7cde6b2f7d01e97
Harsh Singh <harshisbeast@gmail.com>
2025-05-13 17:18:01 +0530

    Remove external image causing runtime error in Sidebar

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,3 +1,5 @@
+// "use client"
+
 import React from "react";
 import { FaUpload } from "react-icons/fa6";
 import { CldUploadWidget, CldUploadWidgetResults } from "next-cloudinary";
@@ -6,28 +8,27 @@ interface FileUploadProps {
   onChange: (url?: string) => void;
 }
 
-const FileUpload = ({ onChange }: FileUploadProps) => {
+const FileUpload = ({onChange}: FileUploadProps) => {
   const handleUpload = (result: CldUploadWidgetResults) => {
     const info = result.info as object;
 
-    // Check if the result contains the necessary fields
-    if ("secure_url" in info && "public_id" in info) {
+    if("secure_url" in info && "public_id" in info) {
       const url = info.secure_url as string;
-      onChange(url); // Send the URL back to the parent component
+      // const public_id = info.public_id as string;
+      onChange(url);
     }
-  };
+  }
 
   return (
     <CldUploadWidget
-      cloudName="ddp30kyze"  // Use your correct Cloud Name here
-      uploadPreset="uploads_preset_123"  // Your upload preset
+      uploadPreset="scnqv8gr"
       options={{ sources: ["local", "url", "unsplash"] }}
       onUpload={handleUpload}
     >
       {({ open }) => {
         return (
           <button
-            className="mt-4 p-4 border rounded-xl bg-background border-dotted dark:border-solid min-h-[200px] flex items-center justify-center cursor-pointer w-full hover:bg-accent/50 duration-300 transition-all"
+            className="mt-4 p-4 border rounded-xl bg-background border-dotted dark:border-solid min-h-[200px] flex items-center justify-center cursor-pointer w-full hover:bg-accent/50 duration-300 transition-all" 
             onClick={() => open()}
           >
             <FaUpload className="text-4xl text-muted-foreground" />
